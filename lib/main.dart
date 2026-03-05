921c72f909e2a03386476e4070ecb35f1d93e4b2
Sconl <sconl@proton.me>
2026-03-05 16:23:27 +0300

    test(actions): testing whether changes deploy

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DEV TEST',
+      title: 'testing development',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
