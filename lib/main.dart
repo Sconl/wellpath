94e6c033e9cfff23b25ac093281e37d2737c02a2
Sconl <sconl@proton.me>
2026-03-06 05:37:16 +0300

    refactor(animated-background): extracted reusable background
    the animated background can now be reused throught the app for wellpath
    
    REFACTOR

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'testing development',
+      title: 'Wellpath | Wellcome',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
